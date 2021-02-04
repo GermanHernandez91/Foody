54bb65006a0a158e46268322d48d706588a0bc4d
Nito <german220291@gmail.com>
2021-02-04 12:37:55 +0000

    Added Constants.kt and created folders to organise the project

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "cc477dd1b5bc450c91cebc1978d246d3"
+    }
+}
