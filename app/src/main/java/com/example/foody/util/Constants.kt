0e9b01edfdacca379f11e0209cb4cb9145a3f9af
Nito <german220291@gmail.com>
2021-02-09 14:57:50 +0000

    Hide api-key

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
     companion object {
 
         const val BASE_URL = "https://api.spoonacular.com"
-        const val API_KEY = "cc477dd1b5bc450c91cebc1978d246d3"
+        const val API_KEY = "[YOUR-API-KEY]"
 
         // API Query Keys
         const val QUERY_NUMBER = "number"
